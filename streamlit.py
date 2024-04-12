47b4f6aeb3e5e114861aaa9e52cb9dcad3b8b598
José Wilson <104285363+josewilsonsouza@users.noreply.github.com>
2024-04-12 03:28:09 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -5,27 +5,99 @@ import matplotlib.pyplot as plt
 
 # Título do aplicativo
 st.title('PBAER')
-st.write('alguma coisa')
-
-arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
-
-st.pyplot(fig)
-
-
-with st.container():
-    st.subheader("Teste com o Streamlit")
-    st.title("Dashboard do Projeto ")
+st.sidebar.image('logo_ufrj.png', use_column_width=True)
 
 @st.cache_data
 def carregar_dados():
     tabela = pd.read_csv('Media_EVA_RET_SUC_por_CENTRO.csv', sep=';', encoding='utf-8')
     return tabela
 
-with st.container():
-    st.write("---")
-    centros = st.selectbox("Selecione o CENTRO", ['CCMN','CT'])
-    dados = carregar_dados() 
-    dados = dados.loc[dados.CENTRO == centros].copy()
-    st.scatter_chart(dados, x="CO_ANO", y="EVASAO")
+def grafico_GERAL(df):
+    '''
+    Função que Plota o gráfico do Centro selecionado
+
+    Parameters
+    ----------
+    df : dataframe.
+
+    Returns
+    -------
+    grafico.
+
+    '''
+    with st.container():
+        
+        st.header("Análise GERAL")
+    
+        st.write("---")
+        centros = st.selectbox("Selecione o CENTRO", sorted(df['CENTRO'].unique()))
+        df_filter = df.loc[df.CENTRO == centros].copy()
+        
+        indicadores = ['EVASAO', 'RETENCAO', 'SUCESSO']
+        labels = ['Evasão', 'Retenção', 'Sucesso']
+        
+        fig, ax = plt.subplots(figsize=(7,4))
+        x = df_filter['CO_ANO']
+        
+        for ind, label in zip(indicadores, labels):
+            ax.plot(x, df_filter[ind], 'o-', label = label, antialiased=True)
+            
+            y_ticks = np.arange(0, 110, 10)
+            x_ticks = np.arange(2010, 2023, 1)
+            ax.set_xticks(x_ticks)
+            ax.set_yticks(y_ticks)
+            ax.set_xlabel('Ano')
+            ax.set_ylabel('%')
+            ax.legend()
+            ax.grid(True, linestyle='--', alpha=0.7)
+        st.pyplot(fig)
+
+
+def grafico_RedeOrigem_CURSOS(df):
+    '''
+    Função que Plota o gráfico do curso selecionado
+
+    Parameters
+    ----------
+    df : dataframe.
+
+    Returns
+    -------
+    grafico.
+
+    '''
+    with st.container():
+        
+        st.header("Análise GERAL")
+    
+        st.write("---")
+        centros = st.selectbox("Selecione o CENTRO", sorted(df['CENTRO'].unique()))
+        df_filter = df.loc[df.CENTRO == centros].copy()
+        
+        indicadores = ['EVASAO', 'RETENCAO', 'SUCESSO']
+        labels = ['Evasão', 'Retenção', 'Sucesso']
+        
+        fig, ax = plt.subplots(figsize=(7,4))
+        x = df_filter['CO_ANO']
+        
+        for ind, label in zip(indicadores, labels):
+            ax.plot(x, df_filter[ind], 'o-', label = label, antialiased=True)
+            
+            y_ticks = np.arange(0, 110, 10)
+            x_ticks = np.arange(2010, 2023, 1)
+            ax.set_xticks(x_ticks)
+            ax.set_yticks(y_ticks)
+            ax.set_xlabel('Ano')
+            ax.set_ylabel('%')
+            ax.legend()
+            ax.grid(True, linestyle='--', alpha=0.7)
+        st.pyplot(fig)
+
+recorte = st.sidebar.radio("RECORTES", ["Por Rede de Origem", "Por Cota","Por Etnia",'Sem recorte'])
+# Determinar qual seção exibir
+df = carregar_dados_CENTROS()
+if recorte == "Sem recorte":
+    grafico_GERAL(df)
+    
+elif recorte == "Por Cota":
+    st.write("Conteúdo de COTA aqui...")
